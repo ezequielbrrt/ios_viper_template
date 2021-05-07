b8e9572ce0a21fb8f85d370392fb878a56bbed1d
Ezequiel Barreto <ezequielbrrt@gmail.com>
2021-05-07 10:39:23 -0500

    Protocol update class to AnyObject

--- VIPEREB.xctemplate/___FILEBASENAME___Protocols.swift
+++ VIPEREB.xctemplate/___FILEBASENAME___Protocols.swift
@@ -9,39 +9,39 @@
 import Foundation
 import UIKit
 
-protocol ___VARIABLE_ModuleName___ViewProtocol: class {
+protocol ___VARIABLE_ModuleName___ViewProtocol: AnyObject {
     var presenter: ___VARIABLE_ModuleName___PresenterProtocol? { get set }
 }
 
-protocol ___VARIABLE_ModuleName___InteractorInputProtocol: class {
+protocol ___VARIABLE_ModuleName___InteractorInputProtocol: AnyObject {
     var presenter: ___VARIABLE_ModuleName___InteractorOutputProtocol? { get set }
     var localDatamanager: ___VARIABLE_ModuleName___LocalDataManagerInputProtocol? { get set }
     var remoteDatamanager: ___VARIABLE_ModuleName___RemoteDataManagerInputProtocol? { get set }
 }
 
-protocol ___VARIABLE_ModuleName___InteractorOutputProtocol: class {
+protocol ___VARIABLE_ModuleName___InteractorOutputProtocol: AnyObject {
 }
 
-protocol ___VARIABLE_ModuleName___RemoteDataManagerInputProtocol: class {
+protocol ___VARIABLE_ModuleName___RemoteDataManagerInputProtocol: AnyObject {
     // INTERACTOR -> REMOTEDATAMANAGER
     var remoteRequestHandler: ___VARIABLE_ModuleName___RemoteDataManagerOutputProtocol? { get set }
 }
 
-protocol ___VARIABLE_ModuleName___RemoteDataManagerOutputProtocol: class {
+protocol ___VARIABLE_ModuleName___RemoteDataManagerOutputProtocol: AnyObject {
     // REMOTEDATAMANAGER -> INTERACTOR
 }
 
-protocol ___VARIABLE_ModuleName___LocalDataManagerInputProtocol: class {
+protocol ___VARIABLE_ModuleName___LocalDataManagerInputProtocol: AnyObject {
     // INTERACTOR -> DATAMANAGER
     var remoteRequestHandler: ___VARIABLE_ModuleName___LocalDataManagerOutputProtocol? { get set }
 }
 
 
-protocol ___VARIABLE_ModuleName___LocalDataManagerOutputProtocol: class {
+protocol ___VARIABLE_ModuleName___LocalDataManagerOutputProtocol: AnyObject {
     // DATAMANAGER -> INTERACTOR
 }
 
-protocol ___VARIABLE_ModuleName___PresenterProtocol: class {
+protocol ___VARIABLE_ModuleName___PresenterProtocol: AnyObject {
     var view: ___VARIABLE_ModuleName___ViewProtocol? { get set }
     var interactor: ___VARIABLE_ModuleName___InteractorInputProtocol? { get set }
     var router: ___VARIABLE_ModuleName___RouterProtocol? { get set }
@@ -49,7 +49,7 @@ protocol ___VARIABLE_ModuleName___PresenterProtocol: class {
     func viewDidLoad()
 }
 
-protocol ___VARIABLE_ModuleName___RouterProtocol: class {
+protocol ___VARIABLE_ModuleName___RouterProtocol: AnyObject {
     static func create___VARIABLE_ModuleName___Module() -> UIViewController
 
     var view: ___VARIABLE_ModuleName___View? { get set }
