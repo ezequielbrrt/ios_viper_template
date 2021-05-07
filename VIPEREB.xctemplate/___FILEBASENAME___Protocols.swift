718cf79b1cf43311fc1c7c8e63ccaeb6db8ec369
Ezequiel Barreto <ezequielbrrt@gmail.com>
2021-05-06 20:38:43 -0500

    New router configuration

--- VIPEREB.xctemplate/___FILEBASENAME___Protocols.swift
+++ VIPEREB.xctemplate/___FILEBASENAME___Protocols.swift
@@ -10,17 +10,14 @@ import Foundation
 import UIKit
 
 protocol ___VARIABLE_ModuleName___ViewProtocol: class {
-    // PRESENTER -> VIEW
     var presenter: ___VARIABLE_ModuleName___PresenterProtocol? { get set }
 }
 
 protocol ___VARIABLE_ModuleName___RouterProtocol: class {
-    // PRESENTER -> ROUTER
     static func create___VARIABLE_ModuleName___Module() -> UIViewController
 }
 
 protocol ___VARIABLE_ModuleName___PresenterProtocol: class {
-    // VIEW -> PRESENTER
     var view: ___VARIABLE_ModuleName___ViewProtocol? { get set }
     var interactor: ___VARIABLE_ModuleName___InteractorInputProtocol? { get set }
     var router: ___VARIABLE_ModuleName___RouterProtocol? { get set }
@@ -29,11 +26,9 @@ protocol ___VARIABLE_ModuleName___PresenterProtocol: class {
 }
 
 protocol ___VARIABLE_ModuleName___InteractorOutputProtocol: class {
-// INTERACTOR -> PRESENTER
 }
 
 protocol ___VARIABLE_ModuleName___InteractorInputProtocol: class {
-    // PRESENTER -> INTERACTOR
     var presenter: ___VARIABLE_ModuleName___InteractorOutputProtocol? { get set }
     var localDatamanager: ___VARIABLE_ModuleName___LocalDataManagerInputProtocol? { get set }
     var remoteDatamanager: ___VARIABLE_ModuleName___RemoteDataManagerInputProtocol? { get set }
--- VIPEREB.xctemplate/___FILEBASENAME___Router.swift
+++ VIPEREB.xctemplate/___FILEBASENAME___Router.swift
@@ -12,30 +12,27 @@ import UIKit
 class ___VARIABLE_ModuleName___Router: ___VARIABLE_ModuleName___RouterProtocol {
 
     class func create___VARIABLE_ModuleName___Module() -> UIViewController {
-        let navController = mainStoryboard.instantiateViewController(withIdentifier: "___VARIABLE_ModuleName___View")
-        if let view = navController.children.first as? ___VARIABLE_ModuleName___View {
-            let presenter: ___VARIABLE_ModuleName___PresenterProtocol & ___VARIABLE_ModuleName___InteractorOutputProtocol = ___VARIABLE_ModuleName___Presenter()
-            let interactor: ___VARIABLE_ModuleName___InteractorInputProtocol & ___VARIABLE_ModuleName___RemoteDataManagerOutputProtocol = ___VARIABLE_ModuleName___Interactor()
-            let localDataManager: ___VARIABLE_ModuleName___LocalDataManagerInputProtocol = ___VARIABLE_ModuleName___LocalDataManager()
-            let remoteDataManager: ___VARIABLE_ModuleName___RemoteDataManagerInputProtocol = ___VARIABLE_ModuleName___RemoteDataManager()
-            let router: ___VARIABLE_ModuleName___RouterProtocol = ___VARIABLE_ModuleName___Router()
-            
-            view.presenter = presenter
-            presenter.view = view
-            presenter.router = router
-            presenter.interactor = interactor
-            interactor.presenter = presenter
-            interactor.localDatamanager = localDataManager
-            interactor.remoteDatamanager = remoteDataManager
-            remoteDataManager.remoteRequestHandler = interactor
-            
-            return navController
-        }
-        return UIViewController()
-    }
-    
-    static var mainStoryboard: UIStoryboard {
-        return UIStoryboard(name: "___VARIABLE_ModuleName___View", bundle: Bundle.main)
+
+        let view: ___VARIABLE_ModuleName___View = ___VARIABLE_ModuleName___View()
+        let presenter: ___VARIABLE_ModuleName___PresenterProtocol & ___VARIABLE_ModuleName___InteractorOutputProtocol = ___VARIABLE_ModuleName___Presenter()
+        let interactor: ___VARIABLE_ModuleName___InteractorInputProtocol & ___VARIABLE_ModuleName___RemoteDataManagerOutputProtocol = ___VARIABLE_ModuleName___Interactor()
+        let localDataManager: ___VARIABLE_ModuleName___LocalDataManagerInputProtocol = ___VARIABLE_ModuleName___LocalDataManager()
+        let remoteDataManager: ___VARIABLE_ModuleName___RemoteDataManagerInputProtocol = ___VARIABLE_ModuleName___RemoteDataManager()
+        let router: ___VARIABLE_ModuleName___RouterProtocol = ___VARIABLE_ModuleName___Router()
+        
+        view.presenter = presenter
+        presenter.view = view
+        presenter.router = router
+        presenter.interactor = interactor
+        interactor.presenter = presenter
+        interactor.localDatamanager = localDataManager
+        interactor.remoteDatamanager = remoteDataManager
+        remoteDataManager.remoteRequestHandler = interactor
+        
+        let navController = UINavigationController()
+        navController.viewControllers.append(view)
+        
+        return view
     }
     
 }
