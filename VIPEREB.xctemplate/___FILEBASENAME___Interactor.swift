8d4c6ffc26f6601321a31ec7c5c9c65d79853a23
Ezequiel Barreto <>
2023-06-19 14:44:25 -0600

    localDataManager fixed on router

--- VIPEREB.xctemplate/___FILEBASENAME___Interactor.swift
+++ VIPEREB.xctemplate/___FILEBASENAME___Interactor.swift
@@ -15,13 +15,10 @@ final class ___VARIABLE_ModuleName___Interactor: ___VARIABLE_ModuleName___Intera
     weak var presenter: ___VARIABLE_ModuleName___InteractorOutputProtocol?
     var localDatamanager: ___VARIABLE_ModuleName___LocalDataManagerInputProtocol?
     var remoteDatamanager: ___VARIABLE_ModuleName___RemoteDataManagerInputProtocol?
-
 }
 
 extension ___VARIABLE_ModuleName___Interactor: ___VARIABLE_ModuleName___RemoteDataManagerOutputProtocol {
-    // TODO: Implement use case methods
 }
 
 extension ___VARIABLE_ModuleName___Interactor: ___VARIABLE_ModuleName___LocalDataManagerOutputProtocol {
-    // TODO: Implement use case methods
 }
--- VIPEREB.xctemplate/___FILEBASENAME___Router.swift
+++ VIPEREB.xctemplate/___FILEBASENAME___Router.swift
@@ -15,7 +15,9 @@ class ___VARIABLE_ModuleName___Router: ___VARIABLE_ModuleName___RouterProtocol {
 
         let view: ___VARIABLE_ModuleName___View = ___VARIABLE_ModuleName___View()
         let presenter: ___VARIABLE_ModuleName___PresenterProtocol & ___VARIABLE_ModuleName___InteractorOutputProtocol = ___VARIABLE_ModuleName___Presenter()
-        let interactor: ___VARIABLE_ModuleName___InteractorInputProtocol & ___VARIABLE_ModuleName___RemoteDataManagerOutputProtocol = ___VARIABLE_ModuleName___Interactor()
+        let interactor: ___VARIABLE_ModuleName___InteractorInputProtocol 
+        & ___VARIABLE_ModuleName___LocalDataManagerOutputProtocol
+        & ___VARIABLE_ModuleName___RemoteDataManagerOutputProtocol = ___VARIABLE_ModuleName___Interactor()
         let localDataManager: ___VARIABLE_ModuleName___LocalDataManagerInputProtocol = ___VARIABLE_ModuleName___LocalDataManager()
         let remoteDataManager: ___VARIABLE_ModuleName___RemoteDataManagerInputProtocol = ___VARIABLE_ModuleName___RemoteDataManager()
         let router: ___VARIABLE_ModuleName___RouterProtocol = ___VARIABLE_ModuleName___Router()
